b92b467a3a4c1331d990650ed048a3c57a2e29f5
vadymmerezhko <vadymm@gmail.com>
2023-12-28 20:27:22 -0800

    Waiter is fixed.

--- src/main/java/org/example/utils/Waiter.java
+++ src/main/java/org/example/utils/Waiter.java
@@ -1,11 +1,11 @@
 package org.example.utils;
 
-import java.util.concurrent.TimeUnit;
+
 
 public class Waiter {
     public static void waitSeconds(int seconds) {
         try {
-            TimeUnit.SECONDS.sleep(seconds);
+            Thread.sleep(seconds * 1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -13,7 +13,7 @@ public class Waiter {
 
     public static void waitMilliSeconds(int milliSeconds) {
         try {
-            TimeUnit.MICROSECONDS.sleep(milliSeconds);
+            Thread.sleep(milliSeconds);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
