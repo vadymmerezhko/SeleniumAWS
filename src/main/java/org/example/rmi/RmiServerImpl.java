a124afb52483665863fbcb9cfee21a020a729839
vadymmerezhko <vadymm@gmail.com>
2024-04-05 14:05:31 -0700

    Multithreading was added for RMI server.

--- src/main/java/org/example/rmi/RmiServerImpl.java
+++ src/main/java/org/example/rmi/RmiServerImpl.java
@@ -22,13 +22,13 @@ public class RmiServerImpl extends UnicastRemoteObject implements RmiServer {
 
         try {
             for (int i = 1; i <= THREAD_COUNT; i++) {
-                String serverName = getRmiServerName(i);
-                int port = getRmiServerPort(i);
-                System.setProperty("java.rmi.server.hostname", "52.53.235.20");
-                RmiServer server = new RmiServerImpl();
-                Registry registry = LocateRegistry.createRegistry(port);
-                registry.rebind(serverName, server);
-                System.out.printf("RMI Test Server has been registered: %s%n", serverName);
+                Thread sessionThread = new Thread() {
+                    public void run() {
+                        System.out.println("RMI server thread id: " + Thread.currentThread().threadId());
+                        registerRmiServer();
+                    }
+                };
+                sessionThread.start();
             }
         }
         catch (Exception e) {
@@ -48,5 +48,22 @@ public class RmiServerImpl extends UnicastRemoteObject implements RmiServer {
 
     public static int getRmiServerPort(int index) {
         return RMI_REGISTRY_PORT + index;
+    }
+
+    private static void registerRmiServer() {
+        try {
+            for (int i = 1; i <= THREAD_COUNT; i++) {
+                String serverName = getRmiServerName(i);
+                int port = getRmiServerPort(i);
+                System.setProperty("java.rmi.server.hostname", "54.193.117.155");
+                RmiServer server = new RmiServerImpl();
+                Registry registry = LocateRegistry.createRegistry(port);
+                registry.rebind(serverName, server);
+                System.out.printf("RMI Test Server has been registered: %s%n", serverName);
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
