d8c85f4ab8b9fa5c04e00b0e736ce711481fce12
vadymmerezhko <vadymm@gmail.com>
2024-04-05 18:18:37 -0700

    RMI server IP was updated.

--- src/main/java/org/example/rmi/RmiServerImpl.java
+++ src/main/java/org/example/rmi/RmiServerImpl.java
@@ -57,7 +57,7 @@ public class RmiServerImpl extends UnicastRemoteObject implements RmiServer {
             System.out.println("RMI Server name: " + serverName);
             System.out.println("RMI port: " + port);
 
-            System.setProperty("java.rmi.server.hostname", "13.52.235.50");
+            System.setProperty("java.rmi.server.hostname", "54.151.3.64");
             RmiServer server = new RmiServerImpl();
             Registry registry = LocateRegistry.createRegistry(port);
             registry.rebind(serverName, server);
