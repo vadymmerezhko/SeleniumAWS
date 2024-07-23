aa853a8370ebf70a3bf071f3674399b7f1052012
vadymmerezhko <vadymm@gmail.com>
2024-07-23 13:19:13 -0700

    FileSystemUtils tests were added and improved.

--- src/test/java/org/example/unit/FileSystemUtilsTest.java
+++ src/test/java/org/example/unit/FileSystemUtilsTest.java
@@ -3,6 +3,8 @@ package org.example.unit;
 import org.example.utils.FileSystemUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.io.IOException;
@@ -31,6 +33,67 @@ public class FileSystemUtilsTest {
     }
 
     @Test
+    public void testCreateFileByFilePath() throws IOException {
+        // Prepare
+        Path tempDir = Files.createTempDirectory("testDir");
+        String fileName = "testFile.txt";
+        String content = "Hello TestNG";
+        String filePath = String.format("%s/%s", tempDir.toString(), fileName);
+        File file = new File(filePath);
+
+        // Execute
+        FileSystemUtils.createFile(file.getPath(), content);
+
+        // Verify
+        Path path = tempDir.resolve(fileName);
+        Assert.assertTrue(Files.exists(path), "File should exist");
+        String fileContent = Files.readString(path);
+        Assert.assertEquals(fileContent, content, "Content should match");
+
+        // Cleanup
+        Files.deleteIfExists(path);
+        Files.deleteIfExists(tempDir);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileByFilePathWrongPath() {
+        FileSystemUtils.createFile("wrong*", "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileByFilePathNullPath() {
+        FileSystemUtils.createFile(null, "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileWrongFolderPath() {
+        FileSystemUtils.createFile("wrong>", "test.txt", "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileNullFolderPath() {
+        FileSystemUtils.createFile(null, "test.txt", "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileWrongFileName() throws IOException {
+        Path tempDir = Files.createTempDirectory("testDir");
+        FileSystemUtils.createFile(tempDir.toString(), "wrong*", "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileBlankFileName() throws IOException {
+        Path tempDir = Files.createTempDirectory("testDir");
+        FileSystemUtils.createFile(tempDir.toString(), "", "Some content");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testCreateFileNullContent() throws IOException {
+        Path tempDir = Files.createTempDirectory("testDir");
+        FileSystemUtils.createFile(tempDir.toString(), "test.txt", null);
+    }
+
+    @Test
     public void testReadFile() throws IOException {
         // Prepare
         Path tempFile = Files.createTempFile("testRead", ".txt");
@@ -47,6 +110,16 @@ public class FileSystemUtilsTest {
         Files.deleteIfExists(tempFile);
     }
 
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testReadFileWrongFilePath() {
+        FileSystemUtils.readFile("wrong*");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testReadFileBlankFilePath() {
+        FileSystemUtils.readFile(" ");
+    }
+
     @Test
     public void testDeleteFile() throws IOException {
         // Prepare
@@ -59,6 +132,17 @@ public class FileSystemUtilsTest {
         Assert.assertFalse(Files.exists(tempFile), "File should be deleted");
     }
 
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testDeleteFileWrongFilePath() {
+        FileSystemUtils.deleteFile("wrong*");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testDeleteFileBlankFilePath() {
+        FileSystemUtils.deleteFile("");
+    }
+
+
     @Test
     public void testFileExists() throws IOException {
         // Prepare
@@ -69,5 +153,15 @@ public class FileSystemUtilsTest {
 
         // Cleanup
         Files.deleteIfExists(tempFile);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testFileWrongFilePathExists() {
+        FileSystemUtils.fileExists("wrong*");
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void testFileBlankFilePathExists() {
+        FileSystemUtils.fileExists(" ");
     }
 }
